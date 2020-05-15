dce55ca937a2dd9c6ff1181fe9d6eb10a32685a1
tombetthauser <tombetthauser@gmail.com>
2020-05-14 23:03:11 -0700

    Gallery name collection for single page working

--- search.py
+++ search.py
@@ -1,63 +1,74 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 from getpass import getpass
 
 
-class LikeBot:
-  def __init__(self, username, password):
-    self.username = username or input("Please input your username: ")
-    self.password = password or getpass(
-        "Please input your password (private / not saved): ")
+class SearchBot:
+  def __init__(self, area=None):
+    self.area = area
     self.driver = webdriver.Chrome()
-    self.driver.get("https://instagram.com")
+    self.driver.get("https://maps.google.com")
+    self.galleries_list = []
     sleep(2)
 
-  def login(self):
-    # fill in username
+  def search_google(self, area=None):
+    # fill and runin search
+    area = area or self.area or input("Please input search area to continue: ")
     login_name = self.driver.find_element_by_xpath(
-        "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input"
-    ).send_keys(self.username)
-
-    # fill in password
-    login_pass = self.driver.find_element_by_xpath(
-        "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input"
-    ).send_keys(self.password)
-    sleep(1)
-
-    # delete password from temporary variable
-    self.password = None
-
-    # click log in button
-    self.driver.find_element_by_xpath(
-        "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]"
-    ).click()
+        "/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/form/div/div[3]/div/input[1]"
+    )
+    login_name.send_keys(f"art gallery, {area}")
+    login_name.send_keys(Keys.RETURN)
     sleep(3)
 
-    # click not now button to close pop up modal
-    self.driver.find_element_by_xpath(
-        "/html/body/div[4]/div/div/div[3]/button[2]"
-    ).click()
-    sleep(1)
+  def retrieve_list(self):
+    classname = "section-result-title-container"
+    resultslist = self.driver.find_elements_by_css_selector(".section-result-title span")
+    for ele in resultslist:
+      inner_text = self.driver.execute_script("return arguments[0].innerText;", ele)
+      self.galleries_list.append(ele)
+      print(f"galleries_list: {len(self.galleries_list)} -- {inner_text}")
+    # # fill in password
+    # login_pass = self.driver.find_element_by_xpath(
+    #     "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input"
+    # ).send_keys(self.password)
+    # sleep(1)
 
-  def fetch_followers(self):  # not yet working
-    # click users profile link
-    self.driver.find_element_by_xpath(
-        "/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[5]/a/img"
-    ).click()
-    sleep(1)
+  #   # delete password from temporary variable
+  #   self.password = None
 
-    # click followers link
-    self.driver.find_element_by_xpath(
-        "/html/body/div[1]/section/main/div/header/section/ul/li[2]/a"
-    ).click()
-    sleep(1)
+  #   # click log in button
+  #   self.driver.find_element_by_xpath(
+  #       "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]"
+  #   ).click()
+  #   sleep(3)
 
-    # scroll to bottom of followers modal
-    modal_bottom = self.driver.find_element_by_xpath(
-        "/html/body/div[4]/div/div[2]/ul/div")
-    followers_ul = self.driver.find_element_by_xpath(
-        "/html/body/div[4]/div/div[2]/ul")
-    followers_ul.click()
+  #   # click not now button to close pop up modal
+  #   self.driver.find_element_by_xpath(
+  #       "/html/body/div[4]/div/div/div[3]/button[2]"
+  #   ).click()
+  #   sleep(1)
+
+  # def fetch_followers(self):  # not yet working
+  #   # click users profile link
+  #   self.driver.find_element_by_xpath(
+  #       "/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[5]/a/img"
+  #   ).click()
+  #   sleep(1)
+
+  #   # click followers link
+  #   self.driver.find_element_by_xpath(
+  #       "/html/body/div[1]/section/main/div/header/section/ul/li[2]/a"
+  #   ).click()
+  #   sleep(1)
+
+  #   # scroll to bottom of followers modal
+  #   modal_bottom = self.driver.find_element_by_xpath(
+  #       "/html/body/div[4]/div/div[2]/ul/div")
+  #   followers_ul = self.driver.find_element_by_xpath(
+  #       "/html/body/div[4]/div/div[2]/ul")
+  #   followers_ul.click()
 
     # self.driver.execute_script("arguments[0].scrollIntoView()", modal_bottom)
     # while (True):
@@ -65,6 +76,6 @@ class LikeBot:
     #   sleep(1)
 
 
-testbot = LikeBot("tombetthauser", "Ch1pD00d3?")
-testbot.login()
-testbot.fetch_followers()
+testbot = SearchBot("San Francisco, CA")
+testbot.search_google()
+testbot.retrieve_list()
