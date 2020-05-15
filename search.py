c8d9b8b7147fab8b4dfcc6a2b06fdfe6cee41df8
tombetthauser <tombetthauser@gmail.com>
2020-05-15 00:47:36 -0700

    Get search loop integrated into class function

--- search.py
+++ search.py
@@ -4,6 +4,7 @@ from time import sleep
 from getpass import getpass
 import os
 
+
 class SearchBot:
   def __init__(self, area=None):
     self.area = area
@@ -15,19 +16,14 @@ class SearchBot:
     print(f"\nNew SearchBot instantiated 🌱\nGoogle maps opened and ready to begin...")
     sleep(2)
 
-  def check_next(self):
-    print("\nChecking for existance of next button...")
-    try:
-      self.driver.find_element_by_class_name("n7lv7yjyC35__button-next-icon")
-      self.continue_search = True
-      print(f"Status of self.continue_search: {self.continue_search}")
-      print(f"Function check_next successfully completed ✓")
-      return True
-    except:
-      self.continue_search = False
-      print(f"Status of self.continue_search: {self.continue_search}")
-      print(f"Function check_next successfully completed ✓")
-      return False
+  def search(self, area=None):
+    area = area or self.area or input(
+        "\nPlease input search area to continue: ")
+    self.search_google("San Francisco, CA")
+    while (self.continue_search == True):
+      self.retrieve_list()
+      self.click_next()
+    print("\nEnd of search loop reached, search complete 👍\n")
 
   def click_next(self):
     print(f"\nInitiating click_next function...")
@@ -35,14 +31,16 @@ class SearchBot:
       nextbutton_class = "n7lv7yjyC35__button-next-icon"
       next_button = self.driver.find_element_by_class_name(nextbutton_class)
       next_button.click()
+      sleep(3)
     except:
       self.continue_search = False
       print(f"Function click_next successfully completed –– no next found ☓")
-    sleep(3)
 
   def search_google(self, area=None):
-    area = area or self.area or input("\nPlease input search area to continue: ")
-    print(f"\nInitiating search_google function with '{area}' for search area...")
+    area = area or self.area or input(
+        "\nPlease input search area to continue: ")
+    print(
+        f"\nInitiating search_google function with '{area}' for search area...")
     login_name = self.driver.find_element_by_xpath(
         "/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/form/div/div[3]/div/input[1]"
     )
@@ -55,19 +53,14 @@ class SearchBot:
     print(f"\nInitiating retrieve_list function call...")
     print(f"Current gallery_list count: {len(self.galleries_list)}\n")
     classname = "section-result-title-container"
-    resultslist = self.driver.find_elements_by_css_selector(".section-result-title span")
+    resultslist = self.driver.find_elements_by_css_selector(
+        ".section-result-title span")
     for ele in resultslist:
-      inner_text = self.driver.execute_script("return arguments[0].innerText;", ele)
+      inner_text = self.driver.execute_script(
+          "return arguments[0].innerText;", ele)
       self.galleries_list.append(ele)
       print(f"galleries_list: {len(self.galleries_list)} -- {inner_text}")
     print(f"Function retrieve_list successfully completed ✓")
 
 testbot = SearchBot()
-testbot.search_google("San Francisco, CA")
-
-while (testbot.continue_search == True):
-  # print(testbot.continue_search)
-  testbot.retrieve_list()
-  testbot.click_next()
-
-print("\nWhile loop exited, search complete 👍\n")
+testbot.search("San Francisco, CA")
