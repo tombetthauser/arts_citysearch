d8b7cdea0baf112722a6574282a0f596c311e4c1
tombetthauser <tombetthauser@gmail.com>
2020-05-15 17:22:39 -0700

    Optimize cut run time and errors with try/excepts for xpath links

--- search.py
+++ search.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from time import sleep
 from getpass import getpass
+from time import sleep
+from time import time
 import os
 
-
 class SearchBot:
-  def __init__(self, area=None):
+  def __init__(self, area="paris, texas"):
     self.area = area
     self.driver = webdriver.Chrome()
     self.galleries_list = []
@@ -16,29 +16,62 @@ class SearchBot:
     self.continue_search = True
     os.system('clear')
     print(f"\nNew SearchBot instantiated 🌱\nGoogle maps opened and ready to begin...")
-    sleep(2)
 
   def search(self, area=None):
-    area = area or self.area or input(
-        "\nPlease input search area to continue: ")
-    self.search_google("San Francisco, CA")
+    area = area or self.area or input("\nPlease input search area to continue: ")
+    self.search_google(area)
+    # sleep(3)
     while (self.continue_search == True):
       self.retrieve_list()
       self.empty_queue()
       self.click_next()
+    self.csv_export()
     print("\nEnd of search loop reached, search complete 👍\n")
+
+  def sanitize_string(self, string):
+    string_copy = string
+    for char in ('(', ')', '"', "'"):
+      string_copy = string_copy.replace(char, '')
+    return string_copy
+    
+  def csv_export(self):
+    print("\nInitiating csv_export function...")
+    unix_time = int(time())
+    file_name = f"data_{unix_time}.csv"
+    os.system(f"touch {file_name}")
+    for gallery_name in self.galleries_list:
+      csv_string = f'"{self.sanitize_string(gallery_name)}",'
+      for info_item in self.galleries_dict[gallery_name]:
+        csv_string += f'"{self.sanitize_string(info_item)}",'
+      csv_string += str(self.area)
+      os.system(f"echo '{csv_string}' >> {file_name}")
+    print(f"Function csv_export successfully completed ✓")
 
   def empty_queue(self):
     print('\nInitiating empty_queue function...')
     for gallery_name in self.galleries_queue:
-      xpath = f'//span[text()="{gallery_name}"]'
-      element = self.driver.find_element_by_xpath(xpath)
+      
+      next_gallery_loaded = False
+      while (next_gallery_loaded == False):
+        try:
+          xpath = f'//span[text()="{gallery_name}"]'
+          element = self.driver.find_element_by_xpath(xpath)
+          next_gallery_loaded = True
+        except:
+          pass
       element.click()
-      sleep(2)
+      # sleep(2)
 
       info = self.driver.find_elements_by_class_name("widget-pane-link")
+      while (len(info) < 1):
+        try:
+          info = self.driver.find_elements_by_class_name("widget-pane-link")
+        except:
+          pass
+
       print(f"\nScraping info for {gallery_name}...")
       print(len(info))
+
       self.galleries_dict[gallery_name] = []
       for ele in list(info):
         inner_text = self.driver.execute_script("return arguments[0].innerText;", ele)
@@ -51,14 +84,22 @@ class SearchBot:
       except:
         description = 'no description'
       self.galleries_dict[gallery_name].append(description)
+
       print("\nGallery info collected:")
       for ele in self.galleries_dict[gallery_name]:
         print(f"– {ele}")
+
       print(f"\nall info added for {gallery_name} ✓")
 
-      back_link = self.driver.find_element_by_xpath("//span[text()='Back to results']")
+      back_link_present = False
+      while (back_link_present == False):
+        try:
+          back_link = self.driver.find_element_by_xpath("//span[text()='Back to results']")
+          back_link_present = True
+        except:
+          pass
       back_link.click()
-      sleep(2)
+
     self.galleries_queue = []
     print(f"Function empty_queue successfully completed ✓")
 
@@ -68,7 +109,6 @@ class SearchBot:
       nextbutton_class = "n7lv7yjyC35__button-next-icon"
       next_button = self.driver.find_element_by_class_name(nextbutton_class)
       next_button.click()
-      sleep(3)
     except:
       self.continue_search = False
       print(f"Function click_next successfully completed –– no next found ☓")
@@ -84,22 +124,25 @@ class SearchBot:
     login_name.send_keys(f"art gallery, {area}")
     login_name.send_keys(Keys.RETURN)
     print(f"Function search_google successfully completed ✓")
-    sleep(3)
 
   def retrieve_list(self):
     print(f"\nInitiating retrieve_list function call...")
     print(f"Current gallery_list count: {len(self.galleries_list)}\n")
-    classname = "section-result-title-container"
-    resultslist = self.driver.find_elements_by_css_selector(
-        ".section-result-title span")
+    resultslist = self.driver.find_elements_by_css_selector(".section-result-title span")
+
+    while (len(resultslist) < 1):
+      try:
+        resultslist = self.driver.find_elements_by_css_selector(".section-result-title span")
+      except:
+        pass
+      
     for ele in resultslist:
-      # append with xcode rather than element object
       inner_text = self.driver.execute_script(
           "return arguments[0].innerText;", ele)
       self.galleries_queue.append(inner_text)
-      self.galleries_list.append(ele)
+      self.galleries_list.append(inner_text)
       print(f"galleries_list: {len(self.galleries_list)} -- {inner_text}")
+
     print(f"Function retrieve_list successfully completed ✓")
 
-testbot = SearchBot()
-testbot.search("San Francisco, CA")
+SearchBot().search()
