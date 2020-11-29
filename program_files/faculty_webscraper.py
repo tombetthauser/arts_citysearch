b7148438a5c1a8c333f500051b2954211fdf35fd
tombetthauser <tombetthauser@gmail.com>
2020-11-28 21:54:28 -0800

    Convert basic google and click to OOP

--- program_files/faculty_webscraper.py
+++ program_files/faculty_webscraper.py
@@ -1,22 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-PATH = "./program_files/chromedriver"
-driver = webdriver.Chrome(PATH)
 
-driver.get("http://google.com")
-print(driver.title)
+class Bot:
+  def __init__(self):
+    self.path = "./program_files/chromedriver"
+    self.driver = webdriver.Chrome(self.path)
+    self.links = []
 
-search = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
-search.send_keys("test")
-search.send_keys(Keys.RETURN)
+  def cycle(self):
+    self.google()
+    self.getlinks()
+    self.clicklink(0)
+    self.quit()
 
-time.sleep(3)
+  def google(self):
+    self.driver.get("http://google.com")
+    search = self.driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+    search.send_keys("test")
+    search.send_keys(Keys.RETURN)
+    time.sleep(3)
 
-searchdiv = driver.find_element_by_id("search")
-links = searchdiv.find_elements_by_tag_name("a")
-links[0].click()
+  def getlinks(self):
+    searchdiv = self.driver.find_element_by_id("search")
+    self.links = searchdiv.find_elements_by_tag_name("a")
+    
+  def clicklink(self, idx):
+    self.links[idx].click()
+    time.sleep(3)
 
-time.sleep(3)
+  def quit(self):
+    self.driver.quit()
 
-driver.quit()
+
+new = Bot()
+new.cycle()
