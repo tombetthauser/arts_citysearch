93b0ff446eecee22f1c18acabf058db863a66e25
tombetthauser <tombetthauser@gmail.com>
2020-11-28 20:34:52 -0800

    Get new scraper basics working

--- program_files/faculty_webscraper.py
+++ program_files/faculty_webscraper.py
@@ -1,5 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from time import sleep
-from time import time
-import os
+PATH = "./program_files/chromedriver"
+driver = webdriver.Chrome(PATH)
+
+driver.get("http://google.com")
+print(driver.title)
+driver.quit()
+
+# from selenium.webdriver.common.keys import Keys
+# from time import sleep
+# from time import time
+# import os
