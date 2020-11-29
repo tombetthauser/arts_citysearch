a8287f7ffa636e8844c504e50a01b741f62c2b56
tombetthauser <tombetthauser@gmail.com>
2020-11-28 21:01:13 -0800

    Get basic search working

--- program_files/faculty_webscraper.py
+++ program_files/faculty_webscraper.py
@@ -1,10 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 PATH = "./program_files/chromedriver"
 driver = webdriver.Chrome(PATH)
 
 driver.get("http://google.com")
 print(driver.title)
-driver.quit()
+
+search = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+# driver.quit()
 
 # from selenium.webdriver.common.keys import Keys
 # from time import sleep
